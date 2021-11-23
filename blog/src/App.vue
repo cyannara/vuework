e07139e02f458640e622d5bda0053e7d7889ea99
cyannara <cyannara@naver.com>
2021-11-24 05:47:51 +0900

    blog

---
+++ blog/src/App.vue
@@ -0,0 +1,29 @@
+<script>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+  import  NavBar from './components/NavBar.vue'
+  import  Profile from './components/Profile.vue'
+  import  Application from './components/Applications.vue'
+export default {
+  name:'App',
+  components: {NavBar, Profile, Application}
+}
+</script>
+
+// <script setup>
+//  import  NavBar from './components/NavBar.vue'
+// </script>
+
+<template>
+  <div>
+    <nav-bar/>
+    <profile/>
+    <application/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
