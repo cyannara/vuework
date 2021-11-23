990994abde03e241afb5d41ce97d13da0f39a6a6
cyannara <cyannara@naver.com>
2021-11-24 05:35:33 +0900

    blig init

---
+++ blog/src/components/Applications.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="row">
+        <div class="col" v-for="appObject in applications_data" :key="appObject.no">
+            <div class="card border-primary mb-3" style="max-width: 18rem;">
+            <div class="card-header">Header</div>
+            <div class="card-body text-primary">
+                <h5 class="card-title">{{appObject.title}}</h5>
+                <p class="card-text">{{appObject.text}}</p>
+            </div>
+            </div>
+        </div>
+    </div>    
+</template>
+
+<script>
+export default {
+    setup() {
+        const applications_data = [
+            {no:1, title:'java', text:'java contents'}, 
+            {no:2, title:'spring', text:'spring contents'},
+            {no:3, title:'vue', text:'vue contents'} 
+        ]
+        return {applications_data}
+    },
+}
+</script>
---
+++ blog/src/components/NavBar.vue
@@ -0,0 +1,37 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item" v-for="menu in menus" :key="menu.key">
+          <a class="nav-link" :class="{active : menu.key==curMenu}" :href="menu.url">{{menu.value}}</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
+<script>
+import { ref } from 'vue'
+export default {
+    name : 'NavVar',
+    setup() {
+        const curMenu = ref('home')
+        const menus = [ 
+            { key: 'home',    value: '홈',        url: '/home' },
+            { key: 'info',    value: '소개',      url: '/info' },
+            { key: 'board',   value: '게시판',    url: '/board'},
+            { key: 'profile', value: 'Profile',   url: '/profile'},
+        ]
+        const onmovePage = (menu_object) => { menu.value = menu_object.key  }
+        return {  curMenu, onmovePage ,menus }
+    },
+}
+</script>
