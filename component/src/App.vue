8b56890ff1e1058995b0d06744263dde54a39636
cyannara <cyannara@naver.com>
2021-11-25 05:19:38 +0900

    component

---
+++ component/src/App.vue
@@ -0,0 +1,23 @@
+import ComponentA from './component/ComponentA.vue'
+<script>
+import ComponentA from './components/ComponenA.vue'
+export default {
+  data(){
+    return {
+      count : 0
+    }
+  }
+  ,
+  components: {   ComponentA  }
+}
+</script>
+
+<template>
+<div>
+  부모 {{count}}
+  <component-a></component-a>
+</div>
+</template>
+
+<style scoped>
+</style>
---
+++ component/src/components/ComponenA.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data(){
+    return {
+      count : 0
+    }
+  }
+}
+</script>
+<template>
+    <div>
+        <h1>component A 입니다.</h1>
+        <button @click="count++">
+            You clicked me {{ count }} times.
+        </button>`
+    </div>
+</template>
