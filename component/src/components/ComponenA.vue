5e3f01418d9cd853e99a114c8525b4044437c161
cyannara <cyannara@naver.com>
2021-11-25 06:00:12 +0900

    emit

--- component/src/components/ComponenA.vue
+++ component/src/components/ComponenA.vue
@@ -1,10 +1,24 @@
 <script>
 export default {
     props: ['msg']
+    ,
+    emits: ['myEvent']
+    ,
+    data(){
+        return {inputCnt : 0}
+    }
+    ,
+    methods : {
+        countChange(){
+            this.$emit('myEvent', this.inputCnt)
+        }
+    }
 }
 </script>
 <template>
     <div>
-        <h1>component A 입니다. {{msg}}</h1>
+        <div>component A 입니다. {{msg}}</div>
+        <input v-model="inputCnt">
+        <button @click="countChange">커스턴이벤트로 데이터 전달</button>
     </div>
 </template>
