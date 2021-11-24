5fac77ceb27499a19c209918e9fc9f7c0c602b57
kym <cyannara@naver.com>
2021-11-24 14:16:47 +0900

    router

--- blog/src/App.vue
+++ blog/src/App.vue
@@ -17,12 +17,32 @@ export default {
 <template>
   <div>
     <nav-bar/>
-    <router-view></router-view>
+    <transition name="bounce">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+
+.bounce-enter-active {
+  animation: bounce-in .5s;
+}
+.bounce-leave-active {
+  animation: bounce-in .5s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
 }
 </style>
