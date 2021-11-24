165a81e245e5095ad7ce249d9b7f5c857228b299
cyannara <cyannara@naver.com>
2021-11-25 06:03:37 +0900

    com0

--- component/src/App.vue
+++ component/src/App.vue
@@ -2,25 +2,13 @@
 import ComponentA from './components/ComponenA.vue'
 export default {
   components: {   ComponentA  }
-  ,
-  data(){
-    return {count : 0 }
-  }
-  ,
-  methods :{
-    countChange(cnt){
-      this.count = cnt;
-    }
-  }
 }
 </script>
 
 <template>
 <div>
-  {{count}}
-  <component-a msg='hi scott' @myEvent="countChange"></component-a>
+  <component-a msg='hi scott'></component-a>
 </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- component/src/components/ComponenA.vue
+++ component/src/components/ComponenA.vue
@@ -1,24 +1,10 @@
 <script>
 export default {
     props: ['msg']
-    ,
-    emits: ['myEvent']
-    ,
-    data(){
-        return {inputCnt : 0}
-    }
-    ,
-    methods : {
-        countChange(){
-            this.$emit('myEvent', this.inputCnt)
-        }
-    }
 }
 </script>
 <template>
     <div>
-        <div>component A 입니다. {{msg}}</div>
-        <input v-model="inputCnt">
-        <button @click="countChange">커스턴이벤트로 데이터 전달</button>
+        <h1>component A 입니다. {{msg}}</h1>
     </div>
 </template>
