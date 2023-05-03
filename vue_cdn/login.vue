80b36f538f0f98c3440b1ede9fd4f0aa5a4ce284
DDOCHI <psh@yedam.ac>
2023-05-03 16:51:42 +0900

    vue cdn

---
+++ vue_cdn/login.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+    <form>
+        <input v-model="member.id">
+        <input v-model="member.pw">
+        <button type="button" @click="login">login</button>
+    <button type="button" @click="gopage">회원가입</button>
+</div>
+</template>
+
+<script>
+module.exports = {
+    data: function() {
+        return {
+            member: {id:'', pw:''}
+        }
+    },
+    methods: {
+        login: function(){
+            console.table( this.member)
+        },
+        gopage : function(){
+            this.$router.push('/signin')
+        },
+    }
+}
+</script>
+
+<style>
+.hello {
+    background-color: #ffe;
+}
+</style>
---
+++ vue_cdn/my-component.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="hello">
+        Hello {{who}}   <p v-html="who">
+    </div>
+</template>
+
+<script>
+module.exports = {
+    data: function() {
+        return {
+            who: 'world!!!'
+        }
+    }
+}
+</script>
+
+<style>
+.hello {
+    background-color: #ffe;
+}
+</style>
