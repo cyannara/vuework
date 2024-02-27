be922115504bb925e77b74c5280fb55f36a8fa8d
kym <cyannara@naver.com>
2024-02-27 15:51:30 +0900

    cli-vue 프로젝트  생성

--- component/src/App.vue
+++ component/src/App.vue
@@ -1,23 +1,24 @@
 <script>
 import ComponentA from './components/ComponenA.vue'
+
 // import {provide } from 'vue'
 // console.log()
 export default {
-  components: {   ComponentA  }
+  components: { ComponentA }
   ,
   //provide : { provideMsg : '프로바이드로 전달' + this.count}
   provide() {
     return {
-      provideMsg : this.user
+      provideMsg: this.user
     }
   }
   ,
-  data(){
-    return {count : 15, user : {id:'', email:''} }
+  data() {
+    return { count: 15, user: { id: '', email: '' } }
   }
   ,
-  methods :{
-    countChange(cnt){
+  methods: {
+    countChange(cnt) {
       this.count = cnt;
     }
   }
@@ -25,11 +26,12 @@ export default {
 </script>
   
 <template>
-<div>
-  {{count}}
-  <component-a msg='hi scott' :count="count" @myEvent="countChange"><div>하위컴포넌트에 slot으로 전달</div></component-a>
-</div>
+  <div>
+    {{ count }}
+    <component-a msg='hi scott' :count="count" @myEvent="countChange">
+      <div>하위컴포넌트에 slot으로 전달</div>
+    </component-a>
+  </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
