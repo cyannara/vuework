9df78501064bc4d80dabbd8a97ef215a94ada570
kym <cyannara@naver.com>
2024-02-27 09:29:36 +0900

    vue2 폴더 정리

---
+++ vue2/vueRouterTest/login.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+    <form>
+        <input name="id">
+        <input name="pw">
+        <button>login</button>
+    </form>
+    <button type="button" @click="gopage">회원가입</button>
+</div>
+</template>
+
+<script>
+module.exports = {
+    data: function() {
+        return {
+            who: 'world'
+        }
+    },
+    methods: {
+        gopage : function(){
+            this.$router.push('/signin')
+        },
+    }
+}
+</script>
+
+<style>
+.hello {
+    background-color: #ffe;
+}
+</style>
